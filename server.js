d38c6009f5c7bb4008e33f281ec6222a52233bd7
Kevin Bielawski <kbielawsk7@gmail.com>
2020-10-04 17:44:53 -0400

    test env vars

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 8080;
 
 app.get("/", (req, res) => {
-  res.send("Hello from express!");
+  res.send(`Hello from express! ${process.env.BLAHBLAHBLAH}`);
 });
 
 app.listen(port, () => {
