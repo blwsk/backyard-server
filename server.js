31ab68cbefa1f0e8bfee1796757e357ecba2f0b0
Kevin Bielawski <kbielawsk7@gmail.com>
2020-10-05 22:09:54 -0400

    stop twilio

--- server.js
+++ server.js
@@ -1,26 +1,9 @@
 const express = require("express");
-const twilio = require("twilio");
 
 const app = express();
 const port = process.env.PORT;
 
-const twilioClient = twilio(
-  process.env.TWILIO_ACCOUNT_SID,
-  process.env.TWILIO_AUTH_TOKEN
-);
-
 app.get("/", (req, res) => {
-  twilioClient.messages
-    .create({
-      body: "Hi Patty",
-      from: "+12109039615",
-      to: "+19089671305",
-    })
-    .then((message) => {
-      console.log(message.sid);
-    });
-  console.log(req.params);
-
   res.send(`Hello from express!`);
 });
 
