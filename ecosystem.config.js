7786a79ae3ed98f356f3104bedddbc290593ce8d
Kevin Bielawski <kbielawsk7@gmail.com>
2020-12-02 08:51:41 -0500

    watch false

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,6 @@ module.exports = {
       name: "backyard_server",
       script: "./server.js",
       logs: "./logs/app.log",
-      watch: true,
       env: {
         PORT: 8080,
         NODE_ENV: "development",
