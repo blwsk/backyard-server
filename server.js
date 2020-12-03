41a9be0b20eedf96f040411a60d776e8c4fc90f3
Kevin Bielawski <kbielawsk7@gmail.com>
2020-12-03 08:28:32 -0500

    cron

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require("express");
+const cron = require("node-cron");
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
   res.send(`Hello from express!`);
 });
 
+cron.schedule("* * * * *", () => {
+  console.log("running a task every minute");
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
