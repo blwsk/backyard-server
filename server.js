438f6b597cfe991ba26c7ce2169638a272e6f02b
Kevin Bielawski <kbielawsk7@gmail.com>
2020-12-08 08:49:27 -0500

    poll

--- server.js
+++ server.js
@@ -1,15 +1,59 @@
 const express = require("express");
 const cron = require("node-cron");
+const fetch = require("isomorphic-unfetch");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const {
+  PORT,
+  AUTH0_CLIENT_ID,
+  AUTH0_CLIENT_SECRET,
+  AUTH0_AUDIENCE,
+  AUTH0_TOKEN_URI,
+  BACKYARD_ROOT_URI,
+} = process.env;
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = PORT;
 
-app.get("/", (req, res) => {
+const getAccessToken = async () => {
+  const options = {
+    method: "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify({
+      client_id: AUTH0_CLIENT_ID,
+      client_secret: AUTH0_CLIENT_SECRET,
+      audience: AUTH0_AUDIENCE,
+      grant_type: "client_credentials",
+    }),
+  };
+
+  const response = await fetch(AUTH0_TOKEN_URI, options);
+
+  const { access_token, token_type, expires_in } = await response.json();
+
+  return { access_token, token_type, expires_in };
+};
+
+app.get("/", async (req, res) => {
   res.send(`Hello from express!`);
 });
 
-cron.schedule("* * * * *", () => {
-  console.log("running a task every minute");
+cron.schedule("0 * * * *", async () => {
+  const { access_token } = await getAccessToken();
+
+  const pollSubsResponse = await fetch(
+    `${BACKYARD_ROOT_URI}/api/rss/poll-subs`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    }
+  ).then((r) => r.json());
+
+  console.log(pollSubsResponse);
 });
 
 app.listen(port, () => {
