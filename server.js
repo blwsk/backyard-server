c642e67ffa876a98364d58b6393280dfef2460ab
Kevin Bielawski <kbielawsk7@gmail.com>
2020-12-11 10:04:56 -0500

    make it work

--- server.js
+++ server.js
@@ -3,47 +3,38 @@ const cron = require("node-cron");
 const fetch = require("isomorphic-unfetch");
 const dotenv = require("dotenv");
 
+const auth = require("./lib/auth");
+const rss = require("./lib/rss");
+const getRecentItems = require("./lib/items");
+
 dotenv.config();
 
-const {
-  PORT,
-  AUTH0_CLIENT_ID,
-  AUTH0_CLIENT_SECRET,
-  AUTH0_AUDIENCE,
-  AUTH0_TOKEN_URI,
-  BACKYARD_ROOT_URI,
-} = process.env;
+const { PORT, BACKYARD_ROOT_URI } = process.env;
 
 const app = express();
 const port = PORT;
-
-const getAccessToken = async () => {
-  const options = {
-    method: "POST",
-    headers: { "content-type": "application/json" },
-    body: JSON.stringify({
-      client_id: AUTH0_CLIENT_ID,
-      client_secret: AUTH0_CLIENT_SECRET,
-      audience: AUTH0_AUDIENCE,
-      grant_type: "client_credentials",
-    }),
-  };
-
-  const response = await fetch(AUTH0_TOKEN_URI, options);
-
-  const { access_token, token_type, expires_in } = await response.json();
-
-  return { access_token, token_type, expires_in };
-};
 
 app.get("/", async (req, res) => {
   res.send(`Hello from express!`);
 });
 
-cron.schedule("0 * * * *", async () => {
-  const { access_token } = await getAccessToken();
+const makeSaveItemsForUser = ({ access_token }) => ({
+  userId,
+  itemsToSave,
+}) => {
+  return fetch(`${BACKYARD_ROOT_URI}/api/rss/bulk-save?userId=${userId}`, {
+    method: "POST",
+    body: JSON.stringify(itemsToSave),
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    },
+  }).then((res) => res.json());
+};
 
-  const pollSubsResponse = await fetch(
+cron.schedule("0 * * * *", async () => {
+  const { access_token } = await auth();
+
+  const { json, before, after, message } = await fetch(
     `${BACKYARD_ROOT_URI}/api/rss/poll-subs`,
     {
       method: "GET",
@@ -53,7 +44,21 @@ cron.schedule("0 * * * *", async () => {
     }
   ).then((r) => r.json());
 
-  console.log(pollSubsResponse);
+  console.log(message);
+
+  const feedManifests = await rss({ json, before, after });
+
+  const recentItems = getRecentItems(feedManifests);
+
+  const saveItemsForUser = makeSaveItemsForUser({ access_token });
+
+  const bulkSaveResult = await Promise.all(
+    recentItems.map(({ userId, itemsToSave }) =>
+      saveItemsForUser({ userId, itemsToSave })
+    )
+  );
+
+  console.log(`Sent ${bulkSaveResult.length} items to backyard.wtf`);
 });
 
 app.listen(port, () => {
